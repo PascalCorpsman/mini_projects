d07b9392cbd44dfc8b3c24cf6edc37283d85b438
PascalCorpsman <corpsman@corpsman.de>
2024-10-01 15:50:22 +0200

    ADD: OpenGLToggleButton ADD: Start adding all LCL-Buttons

--- miniprojects/PixelEditor/upixeleditor.pas
+++ miniprojects/PixelEditor/upixeleditor.pas
@@ -57,16 +57,43 @@ Type
     SaveButton: TOpenGL_Bevel;
     SaveAsButton: TOpenGL_Bevel;
     ExitButton: TOpenGL_Bevel;
+    GridButton: TOpenGL_Bevel;
 
     // Menüleiste Links
     SelectButton: TOpenGL_Bevel;
+    SelectModeButton: TOpenGL_ToggleButton;
     RotateCounterClockwise90: TOpenGL_Bevel;
     RotateClockwise90: TOpenGL_Bevel;
     Rotate180: TOpenGL_Bevel;
     RotateAngle: TOpenGL_Bevel;
+    BrightenButton: TOpenGL_Bevel;
+    DarkenButton: TOpenGL_Bevel;
+    CurserSize1: TOpenGL_Bevel;
+    CurserSize2: TOpenGL_Bevel;
+    CurserSize3: TOpenGL_Bevel;
+    CurserSize4: TOpenGL_Bevel;
+    EraserButton: TOpenGL_Bevel;
+    PencilButton: TOpenGL_Bevel;
+    CursorRoundSize1: TOpenGL_Bevel;
+    CursorRoundSize2: TOpenGL_Bevel;
+    CursorRoundSize3: TOpenGL_Bevel;
+    CursorSquareSize1: TOpenGL_Bevel;
+    CursorSquareSize2: TOpenGL_Bevel;
+    CursorSquareSize3: TOpenGL_Bevel;
+    LineButton: TOpenGL_Bevel;
+    CircleButton: TOpenGL_Bevel;
+    SquareButton: TOpenGL_Bevel;
+    OutlineButton: TOpenGL_Bevel;
+    FilledButton: TOpenGL_Bevel;
+    MirrorButton: TOpenGL_Bevel;
+    MirrorHorButton: TOpenGL_Bevel;
+    MirrorVertButton: TOpenGL_Bevel;
+    Mirror4Button: TOpenGL_Bevel;
+    FloodFillButton: TOpenGL_Bevel;
+    FloodFillModeButton: TOpenGL_Bevel;
+    ColorPickButton: TOpenGL_Bevel;
 
     // Menüleiste unten
-
     SelectLayerButton: TOpenGL_Bevel;
 
     Procedure OnNewButtonClick(Sender: TObject);
@@ -74,16 +101,46 @@ Type
     Procedure OnSaveButtonClick(Sender: TObject);
     Procedure OnSaveAsButtonClick(Sender: TObject);
     Procedure OnExitButtonClick(Sender: TObject);
+    Procedure OnGridButtonClick(Sender: TObject);
 
     Procedure OnSelectButtonClick(Sender: TObject);
     Procedure OnRotateCounterClockwise90ButtonClick(Sender: TObject);
     Procedure OnRotateClockwise90ButtonClick(Sender: TObject);
-
+    Procedure OnRotate180ButtonClick(Sender: TObject);
+    Procedure OnRotateAngleButtonClick(Sender: TObject);
+    Procedure OnBrightenButtonClick(Sender: TObject);
+    Procedure OnDarkenButtonClick(Sender: TObject);
+    Procedure OnCurserSize1ButtonClick(Sender: TObject);
+    Procedure OnCurserSize2ButtonClick(Sender: TObject);
+    Procedure OnCurserSize3ButtonClick(Sender: TObject);
+    Procedure OnCurserSize4ButtonClick(Sender: TObject);
     Procedure OnSelectLayerButtonClick(Sender: TObject);
+    Procedure OnEraserButtonClick(Sender: TObject);
+    Procedure OnPencilButtonClick(Sender: TObject);
+    Procedure OnCursorRoundSize1Click(Sender: TObject);
+    Procedure OnCursorRoundSize2Click(Sender: TObject);
+    Procedure OnCursorRoundSize3Click(Sender: TObject);
+    Procedure OnCursorSquareSize1Click(Sender: TObject);
+    Procedure OnCursorSquareSize2Click(Sender: TObject);
+    Procedure OnCursorSquareSize3Click(Sender: TObject);
+    Procedure OnLineButtonClick(Sender: TObject);
+    Procedure OnCircleButtonClick(Sender: TObject);
+    Procedure OnSquareButtonClick(Sender: TObject);
+    Procedure OnOutlineButtonClick(Sender: TObject);
+    Procedure OnFilledButtonClick(Sender: TObject);
+    Procedure OnMirrorButtonClick(Sender: TObject);
+    Procedure OnMirror4ButtonClick(Sender: TObject);
+    Procedure OnMirrorVertButtonClick(Sender: TObject);
+    Procedure OnMirrorHorButtonClick(Sender: TObject);
+    Procedure OnFloodFillButtonClick(Sender: TObject);
+    Procedure OnFloodFillModeButtonClick(Sender: TObject);
+    Procedure OnColorPickButtonClick(Sender: TObject);
 
     Procedure RenderLCL;
 
     Procedure AddElement(Const value: TOpenGL_BaseClass);
+
+    Procedure NewImage(aWidth, aHeight: Integer);
   public
     FormCloseEvent: TNotifyEvent; // Um der Besitzerklasse mit zu teilen, dass die Anwendung beendet werden will
     Constructor Create; virtual;
@@ -107,52 +164,214 @@ End;
 
 { TPixelEditor }
 
-procedure TPixelEditor.OnNewButtonClick(Sender: TObject);
+Procedure TPixelEditor.OnNewButtonClick(Sender: TObject);
 Begin
 
 End;
 
-procedure TPixelEditor.OnOpenButtonClick(Sender: TObject);
+Procedure TPixelEditor.OnOpenButtonClick(Sender: TObject);
 Begin
 
 End;
 
-procedure TPixelEditor.OnSaveButtonClick(Sender: TObject);
+Procedure TPixelEditor.OnSaveButtonClick(Sender: TObject);
 Begin
 
 End;
 
-procedure TPixelEditor.OnSaveAsButtonClick(Sender: TObject);
+Procedure TPixelEditor.OnSaveAsButtonClick(Sender: TObject);
 Begin
 
 End;
 
-procedure TPixelEditor.OnExitButtonClick(Sender: TObject);
+Procedure TPixelEditor.OnExitButtonClick(Sender: TObject);
 Begin
   If assigned(FormCloseEvent) Then FormCloseEvent(self);
 End;
 
-procedure TPixelEditor.OnSelectButtonClick(Sender: TObject);
+Procedure TPixelEditor.OnGridButtonClick(Sender: TObject);
 Begin
 
 End;
 
-procedure TPixelEditor.OnRotateCounterClockwise90ButtonClick(Sender: TObject);
+Procedure TPixelEditor.OnSelectButtonClick(Sender: TObject);
 Begin
 
 End;
 
-procedure TPixelEditor.OnRotateClockwise90ButtonClick(Sender: TObject);
-begin
-
-end;
-
-procedure TPixelEditor.OnSelectLayerButtonClick(Sender: TObject);
+Procedure TPixelEditor.OnRotateCounterClockwise90ButtonClick(Sender: TObject);
 Begin
 
 End;
 
-procedure TPixelEditor.RenderLCL;
+Procedure TPixelEditor.OnRotateClockwise90ButtonClick(Sender: TObject);
+Begin
+
+End;
+
+Procedure TPixelEditor.OnRotate180ButtonClick(Sender: TObject);
+Begin
+
+End;
+
+Procedure TPixelEditor.OnRotateAngleButtonClick(Sender: TObject);
+Begin
+
+End;
+
+Procedure TPixelEditor.OnBrightenButtonClick(Sender: TObject);
+Begin
+
+End;
+
+Procedure TPixelEditor.OnDarkenButtonClick(Sender: TObject);
+Begin
+
+End;
+
+Procedure TPixelEditor.OnCurserSize1ButtonClick(Sender: TObject);
+Begin
+  CurserSize1.Style := bsRaised;
+  CurserSize2.Style := bsLowered;
+  CurserSize3.Style := bsLowered;
+  CurserSize4.Style := bsLowered;
+End;
+
+Procedure TPixelEditor.OnCurserSize2ButtonClick(Sender: TObject);
+Begin
+  CurserSize1.Style := bsLowered;
+  CurserSize2.Style := bsRaised;
+  CurserSize3.Style := bsLowered;
+  CurserSize4.Style := bsLowered;
+End;
+
+Procedure TPixelEditor.OnCurserSize3ButtonClick(Sender: TObject);
+Begin
+  CurserSize1.Style := bsLowered;
+  CurserSize2.Style := bsLowered;
+  CurserSize3.Style := bsRaised;
+  CurserSize4.Style := bsLowered;
+End;
+
+Procedure TPixelEditor.OnCurserSize4ButtonClick(Sender: TObject);
+Begin
+  CurserSize1.Style := bsLowered;
+  CurserSize2.Style := bsLowered;
+  CurserSize3.Style := bsLowered;
+  CurserSize4.Style := bsRaised;
+End;
+
+Procedure TPixelEditor.OnSelectLayerButtonClick(Sender: TObject);
+Begin
+
+End;
+
+Procedure TPixelEditor.OnEraserButtonClick(Sender: TObject);
+Begin
+
+End;
+
+Procedure TPixelEditor.OnPencilButtonClick(Sender: TObject);
+Begin
+
+End;
+
+Procedure TPixelEditor.OnCursorRoundSize1Click(Sender: TObject);
+Begin
+  CursorRoundSize1.Style := bsRaised;
+  CursorRoundSize2.Style := bsLowered;
+  CursorRoundSize3.Style := bsLowered;
+  CursorSquareSize1.Style := bsLowered;
+  CursorSquareSize2.Style := bsLowered;
+  CursorSquareSize3.Style := bsLowered;
+End;
+
+Procedure TPixelEditor.OnCursorRoundSize2Click(Sender: TObject);
+Begin
+
+End;
+
+Procedure TPixelEditor.OnCursorRoundSize3Click(Sender: TObject);
+Begin
+
+End;
+
+Procedure TPixelEditor.OnCursorSquareSize1Click(Sender: TObject);
+Begin
+
+End;
+
+Procedure TPixelEditor.OnCursorSquareSize2Click(Sender: TObject);
+Begin
+
+End;
+
+Procedure TPixelEditor.OnCursorSquareSize3Click(Sender: TObject);
+Begin
+
+End;
+
+Procedure TPixelEditor.OnLineButtonClick(Sender: TObject);
+Begin
+
+End;
+
+Procedure TPixelEditor.OnCircleButtonClick(Sender: TObject);
+Begin
+
+End;
+
+Procedure TPixelEditor.OnSquareButtonClick(Sender: TObject);
+Begin
+
+End;
+
+Procedure TPixelEditor.OnOutlineButtonClick(Sender: TObject);
+Begin
+
+End;
+
+Procedure TPixelEditor.OnFilledButtonClick(Sender: TObject);
+Begin
+
+End;
+
+Procedure TPixelEditor.OnMirrorButtonClick(Sender: TObject);
+Begin
+
+End;
+
+Procedure TPixelEditor.OnMirror4ButtonClick(Sender: TObject);
+Begin
+
+End;
+
+Procedure TPixelEditor.OnMirrorVertButtonClick(Sender: TObject);
+Begin
+
+End;
+
+Procedure TPixelEditor.OnMirrorHorButtonClick(Sender: TObject);
+Begin
+
+End;
+
+Procedure TPixelEditor.OnFloodFillButtonClick(Sender: TObject);
+Begin
+
+End;
+
+Procedure TPixelEditor.OnFloodFillModeButtonClick(Sender: TObject);
+Begin
+
+End;
+
+Procedure TPixelEditor.OnColorPickButtonClick(Sender: TObject);
+Begin
+
+End;
+
+Procedure TPixelEditor.RenderLCL;
 Var
   i: integer;
 Begin
@@ -183,29 +402,38 @@ Begin
   glVertex2f(641, 425);
   glend();
   glPopMatrix;
+
   // 2. Rendern der Eigentlichen LCL Elemente
   glPushMatrix;
   glTranslatef(0, 0, LayerLCL);
   For i := 0 To high(FElements) Do Begin
     FElements[i].Render();
   End;
-
   glPopMatrix;
 End;
 
-procedure TPixelEditor.AddElement(const value: TOpenGL_BaseClass);
+Procedure TPixelEditor.AddElement(Const value: TOpenGL_BaseClass);
 Begin
   setlength(FElements, high(FElements) + 2);
   FElements[high(FElements)] := value;
 End;
 
-constructor TPixelEditor.Create;
+Procedure TPixelEditor.NewImage(aWidth, aHeight: Integer);
+Begin
+  // Reset aller Curser
+  OnCurserSize1ButtonClick(Nil);
+  OnCursorRoundSize1Click(Nil);
+  OnOutlineButtonClick(Nil);
+  OnMirrorVertButtonClick(Nil);
+End;
+
+Constructor TPixelEditor.Create;
 Begin
   Inherited Create;
   FormCloseEvent := Nil;
 End;
 
-destructor TPixelEditor.Destroy;
+Destructor TPixelEditor.Destroy;
 Var
   i: Integer;
 Begin
@@ -215,14 +443,17 @@ Begin
   setlength(FElements, 0);
 End;
 
-procedure TPixelEditor.MakeCurrent(Owner: TOpenGLControl);
+Procedure TPixelEditor.MakeCurrent(Owner: TOpenGLControl);
 Var
   image: Integer;
 Begin
+
 {$I upixeleditor_constructor.inc}
+
+  NewImage(128, 128);
 End;
 
-procedure TPixelEditor.Render;
+Procedure TPixelEditor.Render;
 Begin
   RenderLCL;
 End;
--- miniprojects/PixelEditor/upixeleditorlcl.pas
+++ miniprojects/PixelEditor/upixeleditorlcl.pas
@@ -6,7 +6,7 @@ Interface
 
 Uses
   Classes, controls, SysUtils, OpenGLContext, uopengl_widgetset,
-  ExtCtrls;
+  uopengl_graphikengine, ExtCtrls;
 
 Type
 
@@ -25,7 +25,21 @@ Type
     Procedure MouseUp(Button: TMouseButton; Shift: TShiftState; X, Y: Integer); override;
 
   public
+    Property Style: TBevelStyle read fStyle write fStyle;
     Constructor Create(Owner: TOpenGLControl); override;
+  End;
+
+  { TOpenGL_ToggleButton }
+
+  TOpenGL_ToggleButton = Class(TOpenGL_Bevel)
+  protected
+    FDownimage: TGraphikItem;
+    Procedure Click; override;
+    Procedure OnRender(); override;
+    Procedure MouseDown(Button: TMouseButton; Shift: TShiftState; X, Y: Integer); override;
+
+  public
+    Procedure SetDownImage(OpenGLIndex: integer); overload;
   End;
 
 Implementation
@@ -54,7 +68,7 @@ Begin
   Else Begin
     glColor3ub($0, $0, $0);
   End;
-  // glLineWidth(max(FOwner.Width / 640, FOwner.Height / 480) * 2);
+  //glLineWidth(max(FOwner.Width / 640, FOwner.Height / 480) * 2);
   glLineWidth(max(FOwner.Width / 640, FOwner.Height / 480) * 1); // Debug, zum Ausmessen der Positionen !
   glBegin(GL_LINE_LOOP);
   glVertex2f(0, 1);
@@ -86,6 +100,49 @@ Begin
   Inherited Create(Owner);
   fStyle := bsLowered;
   fmDown := false;
+End;
+
+{ TOpenGL_ToggleButton }
+
+Procedure TOpenGL_ToggleButton.Click;
+Begin
+  If fStyle = bsLowered Then Begin
+    fStyle := bsRaised;
+  End
+  Else Begin
+    fStyle := bsLowered;
+  End;
+  Inherited Click();
+End;
+
+Procedure TOpenGL_ToggleButton.OnRender;
+Var
+  tmp: TGraphikItem;
+  bool: Boolean;
+Begin
+  bool := fStyle = bsRaised;
+  fStyle := bsLowered;
+  If bool Then Begin
+    tmp := Fimage;
+    Fimage := FDownimage;
+  End;
+  Inherited OnRender();
+  If bool Then Begin
+    fStyle := bsRaised;
+    Fimage := tmp;
+  End;
+End;
+
+Procedure TOpenGL_ToggleButton.MouseDown(Button: TMouseButton;
+  Shift: TShiftState; X, Y: Integer);
+Begin
+  Inherited MouseDown(Button, Shift, X, Y);
+  fmDown := false;
+End;
+
+Procedure TOpenGL_ToggleButton.SetDownImage(OpenGLIndex: integer);
+Begin
+  fDownImage := OpenGL_GraphikEngine.GetInfo(OpenGLIndex);
 End;
 
 End.
