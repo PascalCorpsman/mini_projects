1c8481e1a3b25641979406bbe9117f6f345b2982
PascalCorpsman <corpsman@corpsman.de>
2025-04-13 11:58:37 +0200

    ADD: support for multiple files

--- miniprojects/GIT_gui/Git_Add/unit1.pas
+++ miniprojects/GIT_gui/Git_Add/unit1.pas
@@ -73,20 +73,33 @@ Begin
 End;
 
 Procedure TForm1.FormShow(Sender: TObject);
+Var
+  s: String;
+  sl: TStringList;
+  i: Integer;
+  a: TStringArray;
 Begin
   GitProgress.InitProgressbar(ParamCount);
   GitProgress.Memo1.Clear;
   GitProgress.ModalResult := mrNone;
   GitProgress.SetContinueButtonTo('Commit');
   GitProgress.Show;
-  // TODO: Das sollte auch mit mehr als nur einer Datei gehen ..
-  GitProgress.Memo1.Lines.Add('git add -v "' + trim(ParamStrUTF8(1)) + '"');
-  If GitProgress.RunVisualCommand(ProjectRoot, 'git', ['add', '-v', trim(ParamStrUTF8(1))]) Then Begin
-    close;
-  End
-  Else Begin
-    close;
+  sl := TStringList.create;
+  sl.Text := ParamStrUTF8(1);
+  a := Nil;
+  setlength(a, sl.Count + 3);
+  a[0] := 'add';
+  a[1] := '-v';
+  a[2] := '-f';
+  s := '';
+  For i := 0 To sl.Count - 1 Do Begin
+    a[i + 3] := sl[i];
+    s := s + ' "' + sl[i] + '"';
   End;
+  GitProgress.Memo1.Lines.Add('git add -v -f ' + trim(s));
+  GitProgress.RunVisualCommand(ProjectRoot, 'git', a);
+  sl.free;
+  close;
 End;
 
 End.
